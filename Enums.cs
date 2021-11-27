2e2072e9f56aa26888beb416e8f7505934a12e69
SeyfettinKaratas <77740908+SeyfettinKaratas@users.noreply.github.com>
2021-11-27 23:14:12 +0300

    Add files via upload

---
+++ Enums.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(days.pazar);
+            Console.WriteLine((int)days.pazartesi);
+
+            int sıcaklık = 32;
+            if (sıcaklık<=(int)havaDurumu.normal)
+            {
+                Console.WriteLine("dışarı çıkmak için havanın ısınmasını bkleyelim");
+            }
+            else if (sıcaklık >= (int)havaDurumu.sıcak)
+            {
+                Console.WriteLine("dışarı çıkmak için hava iyi");
+            }
+            else if (sıcaklık >= (int)havaDurumu.normal && sıcaklık<(int)havaDurumu.çoksıcak)
+            {
+                Console.WriteLine("hadi dışarı çıkalım");
+            }
+
+        }
+    }
+
+    enum days
+    {
+        pazartesi,
+        salı,
+        çarşamba,
+        perşembe,
+        cuma=23,
+        cumartesi,
+        pazar
+    }
+
+    enum havaDurumu
+    {
+        soğuk=5,
+        normal=20,
+        sıcak=25,
+        çoksıcak=30
+    }
+}
